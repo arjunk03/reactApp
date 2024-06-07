2603f363eedf93b5225cd7f57e7a50e3f63c5f7f
arjun k <arjunk03@gmail.com>
2024-06-07 17:57:57 +0530

    css updated

--- ReactApp/src/componenets/Fruits.jsx
+++ ReactApp/src/componenets/Fruits.jsx
@@ -1,10 +1,18 @@
 export default function Fruits() {
-  const fruits = ["Apple", "Grapes", "Banana"];
+  //   const fruits = ["Apple", "Grapes", "Banana"];
+  const fruits = [
+    { name: "Apple", price: 10 },
+    { name: "Grapes", price: 20 },
+    { name: "Mango", price: 30 },
+    { name: "Banana", price: 40 },
+  ];
   return (
     <div>
       <ul>
         {fruits.map((fruit) => (
-          <li key={fruit}>{fruit}</li>
+          <li key={fruit.name}>
+            {fruit.name} {fruit.price}
+          </li>
         ))}
       </ul>
     </div>
