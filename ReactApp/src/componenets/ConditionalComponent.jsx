d99e9d5fe7424dfb6a437396f8d44091fc278d32
arjun k <arjunk03@gmail.com>
2024-06-07 18:26:39 +0530

    css updated

--- ReactApp/src/componenets/ConditionalComponent.jsx
+++ ReactApp/src/componenets/ConditionalComponent.jsx
@@ -1,17 +1,26 @@
+import Code from "./Code";
+import Welcome from "./Welcome";
+
 export default function ConditionalComponent() {
-  const display = true;
+  const display = false;
+
+  //   if (display) {
+  //     return (
+  //       <div>
+  //         <h3>This is a Conditional Component </h3>
+  //       </div>
+  //     );
+  //   } else {
+  //     return (
+  //       <div>
+  //         <h3>Code every day</h3>
+  //       </div>
+  //     );
+  //   }
 
   if (display) {
-    return (
-      <div>
-        <h3>This is a Conditional Component </h3>
-      </div>
-    );
+    return <Welcome />;
   } else {
-    return (
-      <div>
-        <h3>Code every day</h3>
-      </div>
-    );
+    return <Code />;
   }
 }
