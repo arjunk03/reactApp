69685e53b5259fa3cbe5cf561a7c3084b42ae78a
arjun k <arjunk03@gmail.com>
2024-06-09 14:59:32 +0530

    css updated

--- todoApp/src/components/Todo.jsx
+++ todoApp/src/components/Todo.jsx
@@ -2,24 +2,26 @@ import { useState } from "react";
 
 export default function Todo() {
   const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
 
   function handleSubmit(e) {
     e.preventDefault();
     // setTodo()
+    setTodos([...todos, todo]);
+    console.log(todos);
   }
 
   return (
     <div>
-      <form>
+      <form onSubmit={(e) => handleSubmit(e)}>
         <input
           onChange={(e) => setTodo(e.target.value)}
           type="text"
           value={todo}
         />
-        <button type="submit" onSubmit={(e) => handleSubmit(e)}>
-          Add
-        </button>
+        <button type="submit">Add</button>
       </form>
+      {console.log(todos)}
     </div>
   );
 }
