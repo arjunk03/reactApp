e63b03a69e4359307f8d84f62fba04d3ae2c94b2
arjun k <arjunk03@gmail.com>
2024-06-09 17:03:14 +0530

    css updated

--- todoApp/src/components/TodoItem.jsx
+++ todoApp/src/components/TodoItem.jsx
@@ -1,5 +1,4 @@
+import "../css/style.css";
 export default function TodoItem({ item }) {
-  const styling = { color: "red" };
-
-  return <h3 style={styling}>{item}</h3>;
+  return <h3 className="header">{item}</h3>;
 }
