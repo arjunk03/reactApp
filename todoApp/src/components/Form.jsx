dc7e37fa65acca385c04ba43cca98ad595758164
arjun k <arjunk03@gmail.com>
2024-06-10 15:40:27 +0530

    css updated

--- todoApp/src/components/Form.jsx
+++ todoApp/src/components/Form.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import styles from "./form.module.css";
 export default function Form({ todos, setTodos }) {
   const [todo, setTodo] = useState("");
 
@@ -10,7 +11,7 @@ export default function Form({ todos, setTodos }) {
     setTodo("");
   }
   return (
-    <form onSubmit={(e) => handleSubmit(e)}>
+    <form className={styles.todoform} onSubmit={(e) => handleSubmit(e)}>
       <input
         onChange={(e) => setTodo(e.target.value)}
         type="text"
