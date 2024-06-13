e1bd9b3fc7d45d6cb69437174526878f5d6664c8
arjun k <arjunk03@gmail.com>
2024-06-13 11:47:23 +0530

    css updated

--- todoApp/src/components/TodoItem.jsx
+++ todoApp/src/components/TodoItem.jsx
@@ -2,13 +2,24 @@ import styles from "./todoitem.module.css";
 export default function TodoItem({ item, todos, setTodos }) {
   function handleDelete(item) {
     console.log(item);
+    // console.log(todo);
 
-    setTodos(todos.filter((todo) => todo !== item));
+    setTodos(todos.filter((todo) => todo.name !== item.name));
+  }
+  function handleClick(name) {
+    // console.log("item clicked");
+    console.log(todos);
+    const newTodos = todos.map((todo) =>
+      todo.name === name ? { ...todos, done: !todo.done } : todo
+    );
+    console.log(newTodos);
+    setTodos(newTodos);
+    console.log(todos);
   }
   return (
     <div className={styles.item}>
       <div className={styles.itemName}>
-        {item.name}
+        <span onClick={() => handleClick(item.name)}>{item.name}</span>
         <span>
           <button
             onClick={() => handleDelete(item)}
