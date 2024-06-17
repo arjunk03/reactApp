d8fd6e232dec65db18f9cdd8ac6b4c11db45db06
arjun k <arjunk03@gmail.com>
2024-06-17 14:58:06 +0530

    todo updated

--- todoApp/src/components/TodoList.jsx
+++ todoApp/src/components/TodoList.jsx
@@ -1,9 +1,12 @@
 import TodoItem from "./TodoItem";
 import styles from "./todolist.module.css";
 export default function TodoList({ todos, setTodos }) {
+  const sortedTodoList = todos
+    .slice()
+    .sort((a, b) => Number(a.done) - Number(b.done));
   return (
     <div className={styles.list}>
-      {todos.map((item) => (
+      {sortedTodoList.map((item) => (
         <TodoItem
           key={item.name}
           item={item}
